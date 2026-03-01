cd2c11cbfb269c99a6ba9ab7f01bf0ff9e3e7ff8
KOMAL <komaldagar18august@gmail.com>
2026-03-01 22:59:16 +0530

    Add files via upload

---
+++ patternquestions/patternproblem4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, row, column;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    for (row = 1; row <= n; row++) {
+        for (column = 1; column <= n; column++) {
+            
+                cout << column;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+/*
+OUTPUT:
+Enter the value of n: 5
+12345
+12345
+12345
+12345
+12345
+
+*/
