764aaa45e14156d69056a433f2725b0561c229c4
KOMAL <komaldagar18august@gmail.com>
2026-03-24 23:34:01 +0530

    Add files via upload

---
+++ patternquestions/primenumber.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    for(i=2;i<n;i++){
+        if(n%i==0){
+            cout<<"not a prime number"<<endl;
+        }
+        else{
+            cout<<"prime number"<<endl;
+        }
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//rapunzelflynn1511
