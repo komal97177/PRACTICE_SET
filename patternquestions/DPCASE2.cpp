ecde45f81f015ec62506df8abc286ca81f608c04
KOMAL <komaldagar18august@gmail.com>
2026-07-04 22:43:02 +0530

    Add files via upload

---
+++ patternquestions/DPCASE2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minSteps(int n) {
+    vector<int> dp(n + 1);
+
+    dp[1] = 0;
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + 1;
+
+        if (i % 2 == 0)
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+
+        if (i % 3 == 0)
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+    }
+
+    return dp[n];
+}
+
+int main() {
+    int n = 10;
+    cout << minSteps(n);
+}
