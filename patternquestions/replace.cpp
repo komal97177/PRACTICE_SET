4292d0e2393ab35727115beeace732d645c12e8a
KOMAL <komaldagar18august@gmail.com>
2026-07-07 22:39:21 +0530

    Add files via upload

---
+++ patternquestions/replace.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+void replacePi(char input[], int i) {
+    // base case
+    // if we reach the end of the string, return
+    if (input[i] == '\0') {
+        return;
+    }
+
+    // if "pi" found
+    if (input[i] == 'p' && input[i + 1] == 'i') {
+
+        // shift string right to make space for "3.14"
+        int j = i + 2;
+        while (input[j] != '\0') {
+            j++;
+        }
+
+        // shift characters right by 2 extra positions
+        for (int k = j; k >= i + 2; k--) {
+            input[k + 2] = input[k];
+        }
+
+        // replace "pi" with "3.14"
+        input[i] = '3';
+        input[i + 1] = '.';
+        input[i + 2] = '1';
+        input[i + 3] = '4';
+
+        // recursive call after replacement
+        replacePi(input, i + 4);
+    }
+    else {
+        // move to next character
+        replacePi(input, i + 1);
+    }
+}
+
+int main() {
+    char str[100] = "pippi";
+
+    replacePi(str, 0);
+
+    cout << str << endl;   // 3.14p3.14
+
+    return 0;
+}
